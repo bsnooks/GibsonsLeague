45be7dd83eb24e43faf320c51f0a1b02d92ca430
Brent Snooks <bsnooks@ehs.com>
2021-04-12 23:22:39 -0600

    Server CORS setup

--- src/server/GibsonsLeague.Api/Startup.cs
+++ src/server/GibsonsLeague.Api/Startup.cs
@@ -1,17 +1,17 @@
+using System;
 using System.Collections.Generic;
-using GibsonsLeague.Data;
+using System.Threading.Tasks;
 using GibsonsLeague.Api.Models;
-using GraphQL.SystemTextJson;
+using GibsonsLeague.Data;
+using GibsonsLeague.Data.Repositories;
 using GraphQL.Server;
-using GraphQL.Server.Ui.Playground;
 using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
+using Microsoft.AspNetCore.Http;
 using Microsoft.EntityFrameworkCore;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
 using Microsoft.Extensions.Hosting;
-using GibsonsLeague.Data.Repositories;
-using System;
 
 namespace GibsonsLeague.Api
 {
@@ -27,6 +27,7 @@ namespace GibsonsLeague.Api
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddCors();
             services.AddDbContext<GLAContext>(options =>
                 options.UseSqlServer(_config.GetConnectionString("GibsonsLeagueDb")));
 
@@ -55,11 +56,12 @@ namespace GibsonsLeague.Api
         // This method gets called by the runtime. Use this method to configure the HTTP request pipeline.
         public void Configure(IApplicationBuilder app, IWebHostEnvironment env)
         {
-            //app.UseCors(builder =>
-            //    builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
+            app.UseCors(builder =>
+                builder.AllowAnyOrigin().AllowAnyHeader().AllowAnyMethod());
             app.UseWebSockets();
             app.UseGraphQLWebSockets<GibsonsLeagueSchema>("/graphql");
             app.UseGraphQL<GibsonsLeagueSchema>();
+
             if (env.IsDevelopment())
             {
                 app.UseGraphQLPlayground(new GraphQL.Server.Ui.Playground.GraphQLPlaygroundOptions());
